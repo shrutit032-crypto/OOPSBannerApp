a37c249a595cd9003dcca9eb4ee350ffc62f9916
shrutit032-crypto <shrutit032@gmail.com>
2026-03-04 01:22:53 +0530

    Completed UC6

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,20 +2,60 @@
 
 public class OOPSBannerApp {
 
+    // Method to generate pattern for letter 'O'
+    public static String[] getOPattern() {
+        return new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        };
+    }
+
+    // Method to generate pattern for letter 'P'
+    public static String[] getPPattern() {
+        return new String[]{
+                "**** ",
+                "*   *",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    ",
+                "*    "
+        };
+    }
+
+    // Method to generate pattern for letter 'S'
+    public static String[] getSPattern() {
+        return new String[]{
+                " ****",
+                "*    ",
+                "*    ",
+                " *** ",
+                "    *",
+                "    *",
+                "**** "
+        };
+    }
+
+    // Main method
     public static void main(String[] args) {
 
-        String[] lines = {
-            String.join("  ", " ***** ", " ***** ", " ******", " ******"),
-            String.join("  ", "*     *", "*     *", "*     *", "*      "),
-            String.join("  ", "*     *", "*     *", "*     *", "*      "),
-            String.join("  ", "*     *", "*     *", " ******", " ******"),
-            String.join("  ", "*     *", "*     *", "*      ", "      *"),
-            String.join("  ", "*     *", "*     *", "*      ", "      *"),
-            String.join("  ", " ***** ", " ***** ", "*      ", " ******")
-        };
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-        for (String line : lines) {
-            System.out.println(line);
+        // Print O O P S
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                    oPattern[i] + "  " +
+                    oPattern[i] + "  " +
+                    pPattern[i] + "  " +
+                    sPattern[i]
+            );
         }
     }
 }
