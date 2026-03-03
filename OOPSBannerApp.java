24fac42cbbe42cad42eeef8faa4d96fd0b3ab872
shrutit032-crypto <shrutit032@gmail.com>
2026-03-03 23:30:03 +0530

    Completed UC1

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,5 @@
-
-public class OOPSBannerApp{
-	public static void main(String[] args){
-		System.out.println("   ***   " + " " + "   ***   " + " " + "******   " + " " + "  *****  ");
-		System.out.println(" **   ** " + " " + " **   ** " + " " + "**    ** " + " " + " **    **");
-		System.out.println("**     **" + " " + "**     **" + " " + "**     **" + " " + "**       ");
-		System.out.println("**     **" + " " + "**     **" + " " + "**    ** " + " " + "  ****   ");
-		System.out.println("**     **" + " " + "**     **" + " " + "******   " + " " + "      ** ");
-		System.out.println(" **   ** " + " " + " **   ** " + " " + "**       " + " " + "**     **");
-		System.out.println("   ***   " + " " + "   ***   " + " " + "**       " + " " + "  *****  ");
-	}
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
 }
